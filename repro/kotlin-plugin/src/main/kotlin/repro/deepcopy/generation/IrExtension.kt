8e7521170b59926cc3f307994295cf4a755ebe0f
Oleksandr.Karpovich <a.n.karpovich@gmail.com>
2024-01-16 12:15:48 +0100

    Add a reproducer for static IrField added to IrClass

--- repro/kotlin-plugin/src/main/kotlin/repro/deepcopy/generation/IrExtension.kt
+++ repro/kotlin-plugin/src/main/kotlin/repro/deepcopy/generation/IrExtension.kt
@@ -2,13 +2,19 @@ package repro.deepcopy.generation
 
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.backend.common.extensions.IrPluginContext
+import org.jetbrains.kotlin.descriptors.DescriptorVisibilities
 import org.jetbrains.kotlin.ir.IrStatement
 import org.jetbrains.kotlin.ir.ObsoleteDescriptorBasedAPI
 import org.jetbrains.kotlin.ir.UNDEFINED_OFFSET
+import org.jetbrains.kotlin.ir.builders.declarations.buildField
 import org.jetbrains.kotlin.ir.declarations.*
 import org.jetbrains.kotlin.ir.expressions.IrCall
+import org.jetbrains.kotlin.ir.expressions.IrConst
+import org.jetbrains.kotlin.ir.expressions.IrConstKind
 import org.jetbrains.kotlin.ir.expressions.IrExpression
+import org.jetbrains.kotlin.ir.expressions.impl.IrConstImpl
 import org.jetbrains.kotlin.ir.expressions.impl.IrConstructorCallImpl
+import org.jetbrains.kotlin.ir.expressions.impl.IrExpressionBodyImpl
 import org.jetbrains.kotlin.ir.types.classifierOrNull
 import org.jetbrains.kotlin.ir.types.defaultType
 import org.jetbrains.kotlin.ir.util.*
@@ -35,47 +41,35 @@ class Transformation(
 
     val annotation = context.referenceClass(annotationToAdd)?.takeIf { it.owner.isAnnotationClass }!!
 
-    override fun visitSimpleFunction(declaration: IrSimpleFunction): IrStatement {
-        // This is applied to module `integration`
-        if (declaration.name.asString().contains("AddAnnotationToThisFun")) {
-            addAnnotation(declaration)
-//            error(declaration.dumpKotlinLike())
+    protected fun irConst(value: Int): IrConst<Int> = IrConstImpl(
+        UNDEFINED_OFFSET,
+        UNDEFINED_OFFSET,
+        context.irBuiltIns.intType,
+        IrConstKind.Int,
+        value
+    )
+
+    fun makeStabilityField(): IrField {
+        return context.irFactory.buildField {
+            startOffset = SYNTHETIC_OFFSET
+            endOffset = SYNTHETIC_OFFSET
+            name = Name.identifier("fieldAddedByPlugin")
+            isStatic = true
+            isFinal = true
+            type = context.irBuiltIns.intType
+            visibility = DescriptorVisibilities.PUBLIC
         }
-        return super.visitSimpleFunction(declaration)
     }
 
-    private fun addAnnotation(declaration: IrSimpleFunction) {
-        val annotationConstructor = annotation.owner.constructors.first()
-        val annotation = IrConstructorCallImpl.fromSymbolOwner(
-            type = annotation.defaultType,
-            constructorSymbol = annotationConstructor.symbol
-        )
-        declaration.annotations += annotation
-        // for Beta1
-//        context.annotationsRegistrar.addMetadataVisibleAnnotationsToElement(declaration, annotation)
-
-        // for Beta2
-         context.metadataDeclarationRegistrar.addMetadataVisibleAnnotationsToElement(declaration, annotation)
-    }
-
-    override fun visitCall(expression: IrCall): IrExpression {
-        val callee = expression.symbol.owner
-        if (callee.name.asString().contains("AddAnnotationToThisFun")) {
-            assertAboutCallFunction(callee)
+    override fun visitClass(declaration: IrClass): IrStatement {
+        declaration.declarations += makeStabilityField().apply {
+            parent = declaration
+            initializer = IrExpressionBodyImpl(
+                UNDEFINED_OFFSET,
+                UNDEFINED_OFFSET,
+                irConst(10)
+            )
         }
-        return super.visitCall(expression)
-    }
-
-    private fun assertAboutCallFunction(declaration: IrSimpleFunction) {
-//        error(declaration.dump())
-        if (declaration.name.asString() == "AddAnnotationToThisFunToo") {
-//            error(declaration.dump())
-        }
-        assert(declaration.annotations.isNotEmpty()) {
-            "Expected non empty annotations. \nActual dump: ${declaration.dump()}"
-        }
-        assert(declaration.annotations.hasAnnotation(annotationToAdd.asSingleFqName())) {
-            "Expected $annotationToAdd. \nActual dump: ${declaration.dump()}"
-        }
+        return super.visitClass(declaration)
     }
 }
